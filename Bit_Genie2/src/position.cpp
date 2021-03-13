5e0b1e4b9f8111ea9adbd3a4213158ce7563b21a
Aryan1508 <68417405+Aryan1508@users.noreply.github.com>
2021-03-13 22:14:54 +0530

    Revert "speed up in move is legal"
    This reverts commit c3d03b98c9006182a4ea9279d267d7d37c34253c.

--- Bit_Genie2/src/position.cpp
+++ Bit_Genie2/src/position.cpp
@@ -259,18 +259,20 @@ bool Position::move_is_legal(Move move)
       }
 
       uint64_t pawns   = pieces.bitboards[Pawn]   & enemy;
+      uint64_t knights = pieces.bitboards[Knight] & enemy;
       uint64_t bishops = pieces.bitboards[Bishop] & enemy;
       uint64_t rooks   = pieces.bitboards[Rook]   & enemy;
       uint64_t queens  = pieces.bitboards[Queen]  & enemy;
 
-      Square king = get_lsb(pieces.bitboards[King] & friend_bb());
+      Square king = get_lsb(pieces.bitboards[King]   & friend_bb());
 
       bishops |= queens;
       rooks   |= queens;
 
       return !((BitMask::pawn_attacks[side][king] & pawns)
         || (Attacks::bishop(king, occupancy) & bishops)
-        || (Attacks::rook(king, occupancy) & rooks));
+        || (Attacks::rook(king, occupancy) & rooks)
+        || (Attacks::knight(king) & knights));
     }
   }
 
@@ -298,7 +300,8 @@ bool Position::move_is_legal(Move move)
 
     return !((BitMask::pawn_attacks[side][king] & pawns)
       || (Attacks::bishop(king, occupancy) & bishops)
-      || (Attacks::rook(king, occupancy) & rooks));
+      || (Attacks::rook(king, occupancy) & rooks)
+      || (Attacks::knight(king) & knights));
   }
 
   return true;
