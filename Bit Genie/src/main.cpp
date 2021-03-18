f676f5826b9387c9a5ac740eda65c3091d3d5012
Aryan1508 <68417405+Aryan1508@users.noreply.github.com>
2021-03-18 15:17:49 +0530

    .

--- Bit Genie/src/main.cpp
+++ Bit Genie/src/main.cpp
@@ -11,43 +11,9 @@
 #include "zobrist.h"
 #include <queue>
 
-void check_pseudolegal(Position& position, int depth)
-{
-	if (depth <= 0)
-		return;
-
-	MoveGenerator gen;
-	gen.generate(position);
-
-	for (auto m : gen.movelist)
-	{
-		if (!position.move_is_pseudolegal(m))
-		{
-			std::cout << position;
-			std::cout << "Error for " << print_move(m) << std::endl;
-			std::cin.get();
-		}
-		position.apply_move(m);
-		check_pseudolegal(position, depth - 1);
-		position.revert_move();
-	}
-}
-
 int main()
 {
 	Attacks::init();
 	ZobristKey::init();
     uci_input_loop();
-	Position position;
-	position.set_fen("rnb1kbnr/ppp1pppp/8/3Q4/4p3/8/PPPP1qPP/RNBK1BNR w kq - 0 1");
-
-	MoveGenerator<false> gen;
-	gen.generate(position);
-
-	for (auto m : gen.movelist)
-		std::cout << print_move(m) << std::endl;
-
-	Move move = CreateMove(D5, E6, MoveFlag::normal, 1);
-	
-	std::cout << "Move is pseudo-legal: " << position.move_is_pseudolegal(move) << std::endl;
 }
