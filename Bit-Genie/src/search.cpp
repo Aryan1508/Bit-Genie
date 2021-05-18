25b25e007e7d5918cd75b1dacca40604cac1b4b8
Aryan1508 <68417405+Aryan1508@users.noreply.github.com>
2021-05-18 10:26:51 +0530

    Bench: 21681126

--- Bit-Genie/src/search.cpp
+++ Bit-Genie/src/search.cpp
@@ -227,7 +227,7 @@ namespace
 
                 if (!move_is_capture(position, move))
                 {
-                    search.history.add(position, move, depth);
+                    search.history.add(position, move, depth * depth);
                     search.history.penalty(position, picker.gen.movelist, move, depth);
                     search.killers.add(search.info.ply, move);
                 }
