676f29bd64dc4005778ec8fc0883c1474f23fef7
Aryan Parekh <68417405+Aryan1508@users.noreply.github.com>
2021-06-22 16:36:49 +0530

    TT size allocation fix (#52)
    Bench: 15874588

--- Bit-Genie/src/uci.cpp
+++ Bit-Genie/src/uci.cpp
@@ -25,7 +25,7 @@
 #include "benchmark.h"
 #include "searchinit.h"
 
-const char *version = "5.75";
+const char *version = "5.6";
 
 namespace
 {
@@ -39,7 +39,7 @@ namespace
     {
         printl("id name Bit-Genie ", version);
         printl("id author Aryan Parekh");
-        printl("option name Hash type spin default 2 min 2 max 3000");
+        printl("option name Hash type spin default 8 min 2 max 3000");
         printl("option name Clear Hash type button");
         printl("uciok");
     }
@@ -135,7 +135,7 @@ void uci_input_loop(int argc, char **argv)
 
     UciParser command;
     Position position;
-    TTable table(2);
+    TTable table(8);
     SearchInit worker;
 
     if (argc > 1 && !strncmp(argv[1], "bench", 5))
