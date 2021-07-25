758b49b119c8fa1b32efd9da04d422ddc94d7717
Aryan1508 <68417405+Aryan1508@users.noreply.github.com>
2021-07-25 16:37:09 +0530

    Bench: 5292392

--- src/search.cpp
+++ src/search.cpp
@@ -215,7 +215,7 @@ namespace
 
     int calculate_sp_margin(int depth)
     {
-        return depth * -60;
+        return depth * -100;
     }    
 
     SearchResult pvs(Search::Info& search, int depth, int alpha, int beta, bool do_null = true)
